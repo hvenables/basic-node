4e0aeb1f8987fdb7ad5762cb2f7310ad826683d2
Harry Venables <venablesh@yahoo.co.uk>
2015-11-28 11:45:55 +0000

    first commit

---
+++ Gruntfile.js
@@ -0,0 +1,19 @@
+module.exports = function(grunt) {
+  pkg: grunt.file.readJSON('package.json'),
+  grunt.initConfig({
+    webdriver: {
+      helloWorld: {
+        tests: 'test/**/*'
+      },
+      options: {
+        desiredCapabilities: {
+          browserName: 'chrome'
+      }
+    },
+  }
+});
+
+  grunt.loadNpmTasks('grunt-webdriver');
+
+  grunt.registerTask('default', ['webdriver']);
+};
